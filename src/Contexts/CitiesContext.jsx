f8ce6cf317f1f3cb79af8d064360d27e92e15654
Raghu <raghubunty5@gmail.com>
2025-08-11 14:52:26 -0500

    Make site mobile responsive 7

--- src/Contexts/CitiesContext.jsx
+++ src/Contexts/CitiesContext.jsx
@@ -7,7 +7,8 @@ import {
 } from "react";
 
 const CitiesContext = createContext();
-const Base_URL = "http://localhost:8080";
+// const Base_URL = "http://localhost:8080";
+const Base_URL = "https://b593194f3249.ngrok-free.app";
 
 const initial_state = {
   cities: [],
