6e7aee534f12c7aa57341ef634a185f2a6d6495e
Raghu <raghubunty5@gmail.com>
2025-08-13 02:28:19 -0500

    Make site mobile responsive 17

--- src/Contexts/CitiesContext.jsx
+++ src/Contexts/CitiesContext.jsx
@@ -7,7 +7,10 @@ import {
 } from "react";
 
 const CitiesContext = createContext();
-const Base_URL = "http://localhost:8080";
+// const Base_URL = "http://localhost:8080";
+const Base_URL =
+  "http://worldwiseapp-env.eba-iav3hwxm.us-east-2.elasticbeanstalk.com";
+
 console.log(Base_URL);
 
 const initial_state = {
