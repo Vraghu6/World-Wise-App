8de255f2b8a3e83ac9dd8652c3f125736d52fd63
Raghu <raghubunty5@gmail.com>
2025-08-14 11:58:32 -0500

    Make site mobile responsive 37

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Homepage from "./Pages/Homepage";
-import Pricing from "./Pages/Pricing";
-import Product from "./Pages/Product";
-import Login from "./Pages/Login";
-import AppLayout from "./Pages/AppLayout";
-import City from "./Components/City";
-import Form from "./Components/Form";
+import { Suspense, lazy } from "react";
+const Homepage = lazy(() => import("./Pages/Homepage"));
+const Pricing = lazy(() => import("./Pages/Pricing"));
+const Product = lazy(() => import("./Pages/Product"));
+const Login = lazy(() => import("./Pages/Login"));
+const AppLayout = lazy(() => import("./Pages/AppLayout"));
+const City = lazy(() => import("./Components/City"));
+const Form = lazy(() => import("./Components/Form"));
 
 import Pagenotfound from "./Pages/Pagenotfound";
 import CityList from "./Components/CityList";
@@ -20,25 +21,26 @@ function App() {
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="product" element={<Product />} />
-            <Route path="login" element={<Login />} />
-            <Route path="signup" element={<Signup />} />
-            <Route path="*" element={<Pagenotfound />} />
-            <Route path="/app" element={<AppLayout />}>
-              <Route index element={<Navigate replace to="cities" />} />
-              <Route path="cities" element={<CityList />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="product" element={<Product />} />
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<Signup />} />
+              <Route path="*" element={<Pagenotfound />} />
+              <Route path="/app" element={<AppLayout />}>
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
   );
 }
-
 export default App;
